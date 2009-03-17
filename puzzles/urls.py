ebfce76b650a7a61e91ecbf3b0148fd7f4768853
Tim Wee <tim.shawn@gmail.com>
2009-03-16 22:06:50 -0700

    add initial welcome page

--- puzzles/urls.py
+++ puzzles/urls.py
@@ -2,7 +2,11 @@ from django.conf.urls.defaults import *
 
 from views import *
 
-urlpatterns = patterns('puzzles.views',
+urlpatterns = patterns('django.views.generic.simple',
+    (r'^$', 'direct_to_template', {'template' : 'index.html'}),
+  )
+
+urlpatterns += patterns('puzzles.views',
   (r'^puzzles$', 'all_puzzles'),
   (r'^puzzle/create', 'create_puzzle'),
   (r'^puzzle/(?P<puzzle_id>\d+)/view$', 'view_puzzle'),
